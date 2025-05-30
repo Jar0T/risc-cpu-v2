5875ddcad4c4eda67e41646e514a9b7c9db31273
Jar0T <jarotumula4@gmail.com>
2025-05-30 17:55:16 +0200

    Added hart module to contain core pipeline modules

---
+++ cpu/hart/hart.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:        Jarosław Tumula
+-- 
+-- Create Date:     17:53:30 05/30/2025 
+-- Design Name: 
+-- Module Name:     hart - RTL 
+-- Project Name:    risc-cpu-v2
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity hart is
+    Port ( i_clk : in  STD_LOGIC;
+           i_reset : in  STD_LOGIC);
+end hart;
+
+architecture RTL of hart is
+
+begin
+
+
+end RTL;
+
