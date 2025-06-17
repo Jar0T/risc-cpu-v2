f4cd945906f32ce5cb522d8c3a149d1886c317e5
Jar0T <jarotumula4@gmail.com>
2025-06-17 23:48:02 +0200

    Changed inputs and output type from signed to std_logic_vector

--- cpu/hart/alu/alu.vhd
+++ cpu/hart/alu/alu.vhd
@@ -31,9 +31,9 @@ entity alu is
         i_clk : in std_logic;
         i_reset : in std_logic;
         i_func : in std_logic_vector(3 downto 0);
-        i_a : in signed(31 downto 0);
-        i_b : in signed(31 downto 0);
-        o_y : out signed(31 downto 0)
+        i_a : in std_logic_vector(31 downto 0);
+        i_b : in std_logic_vector(31 downto 0);
+        o_y : out std_logic_vector(31 downto 0)
     );
 end alu;
 
@@ -46,28 +46,28 @@ architecture Behavioral of alu is
     signal s_shift_left_logical : unsigned(31 downto 0) := (others => '0');
     signal s_set_less_than : signed(31 downto 0) := (others => '0');
     signal s_set_less_than_unsigned : signed(31 downto 0) := (others => '0');
-    signal s_xor : signed(31 downto 0) := (others => '0');
+    signal s_xor : std_logic_vector(31 downto 0) := (others => '0');
     signal s_shift_right_logical : unsigned(31 downto 0) := (others => '0');
     signal s_shift_right_arithmetic : signed(31 downto 0) := (others => '0');
-    signal s_or : signed(31 downto 0) := (others => '0');
-    signal s_and : signed(31 downto 0) := (others => '0');
+    signal s_or : std_logic_vector(31 downto 0) := (others => '0');
+    signal s_and : std_logic_vector(31 downto 0) := (others => '0');
     
     signal s_shift_amount : integer range 0 to 31 := 0;
 
 begin
 
-    o_y <= s_result;
+    o_y <= std_logic_vector(s_result);
     
     s_shift_amount <= to_integer(unsigned(i_b(4 downto 0)));
     
-    s_addition <= i_a + i_b;
-    s_subtraction <= i_a - i_b;
+    s_addition <= signed(i_a) + signed(i_b);
+    s_subtraction <= signed(i_a) - signed(i_b);
     s_shift_left_logical <= shift_left(unsigned(i_a), s_shift_amount);
-    s_set_less_than <= to_signed(1, s_result'length) when i_a < i_b else to_signed(0, s_result'length);
+    s_set_less_than <= to_signed(1, s_result'length) when signed(i_a) < signed(i_b) else to_signed(0, s_result'length);
     s_set_less_than_unsigned <= to_signed(1, s_result'length) when unsigned(i_a) < unsigned(i_b) else to_signed(0, s_result'length);
     s_xor <= i_a xor i_b;
     s_shift_right_logical <= shift_right(unsigned(i_a), s_shift_amount);
-    s_shift_right_arithmetic <= shift_right(i_a, s_shift_amount);
+    s_shift_right_arithmetic <= shift_right(signed(i_a), s_shift_amount);
     s_or <= i_a or i_b;
     s_and <= i_a and i_b;
 
@@ -87,15 +87,15 @@ begin
                     when "001" => s_result <= signed(s_shift_left_logical);
                     when "010" => s_result <= s_set_less_than;
                     when "011" => s_result <= s_set_less_than_unsigned;
-                    when "100" => s_result <= s_xor;
+                    when "100" => s_result <= signed(s_xor);
                     when "101" =>
                         if i_func(3) = '0' then
                             s_result <= signed(s_shift_right_logical);
                         else
                             s_result <= s_shift_right_arithmetic;
                         end if;
-                    when "110" => s_result <= s_or;
-                    when "111" => s_result <= s_and;
+                    when "110" => s_result <= signed(s_or);
+                    when "111" => s_result <= signed(s_and);
                     when others => s_result <= (others => '0');
                 end case;
             end if;
