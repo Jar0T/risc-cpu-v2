13a477ebcc90ec00499b23ac72720853b0eed474
Jar0T <jarotumula4@gmail.com>
2025-06-18 12:36:43 +0200

    Added branch unit test bench

--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "secureip" -o "/home/jarek/sources/rv32i/vhdl/risc-cpu-v2/memory_readback_tb_isim_beh.exe" -prj "/home/jarek/sources/rv32i/vhdl/risc-cpu-v2/memory_readback_tb_beh.prj" "work.memory_readback_tb" 
+-intstyle "ise" -incremental -lib "secureip" -o "/home/jarek/sources/rv32i/vhdl/risc-cpu-v2/branch_unit_tb_isim_beh.exe" -prj "/home/jarek/sources/rv32i/vhdl/risc-cpu-v2/branch_unit_tb_beh.prj" "work.branch_unit_tb" 
